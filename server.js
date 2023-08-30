36fa5c9bdcde94763f631de9f0c3ad1dffdd72fa
Sitraka <73686076+sitrakak@users.noreply.github.com>
2023-08-30 19:20:33 +0300

    essai deploiement

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/assignment-app/"));
+app.use(express.static(__dirname + "/dist/tpt-app/"));
 
 app.get("/*", function (req, res) {
- res.sendFile(path.join(__dirname + "/dist/assignment-app/index.html"));
+ res.sendFile(path.join(__dirname + "/dist/tpt-app/index.html"));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081);
