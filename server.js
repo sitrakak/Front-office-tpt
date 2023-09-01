2193d122652d52fc63511ce0060032be69a547c5
Sitraka <73686076+sitrakak@users.noreply.github.com>
2023-09-01 16:27:38 +0300

    essai depl

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/tpt-app/"));
+app.use(express.static(__dirname + "/dist/Front_office/"));
 
 app.get("/*", function (req, res) {
- res.sendFile(path.join(__dirname + "/dist/tpt-app/src/index.html"));
+ res.sendFile(path.join(__dirname + "/dist/Front_office/index.html"));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081);
