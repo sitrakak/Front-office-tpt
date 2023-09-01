829b84072af92bf183953115f461d1cb079f2206
Sitraka <73686076+sitrakak@users.noreply.github.com>
2023-09-01 16:08:34 +0300

    depl

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist/tpt-app/"));
 
 app.get("/*", function (req, res) {
- res.sendFile(path.join(__dirname + "/dist/tpt-app/index.html"));
+ res.sendFile(path.join(__dirname + "/dist/tpt-app/src/index.html"));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081);
