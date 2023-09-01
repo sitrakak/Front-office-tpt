1fd9f5fb6b356911d1c3d056b42a8b2d14ae6a28
SitrakaH <sitraka.hajanirivo@solar-transit.net>
2023-09-01 09:23:47 +0300

    comment

--- src/app/e-gouvernance/projet/details-projet/details-projet.component.ts
+++ src/app/e-gouvernance/projet/details-projet/details-projet.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/auth.service';
+import { CommentaireService } from 'src/app/shared/commentaire.service';
 import { MembreService } from 'src/app/shared/membre.service';
 import { ProjetService } from 'src/app/shared/projet.service';
 
@@ -22,8 +24,10 @@ export class DetailsProjetComponent implements OnInit{
   showForm: boolean = false;
   etapes: any;
   projet: any;
+  commentaire: any;
+  comment: any= '';
 
-  public constructor(private router:Router, private route:ActivatedRoute, private projetService:ProjetService){}
+  public constructor(private router:Router, private route:ActivatedRoute, private projetService:ProjetService, private commentaireService:CommentaireService, private authService: AuthService){}
 
   ngOnInit(){
     this.route.params.subscribe(params => {
@@ -31,6 +35,7 @@ export class DetailsProjetComponent implements OnInit{
     });
     this.getListEtape();
     this.getListProjet();
+    this.getCommentaire();
   }
 
   getListEtape() {
@@ -94,5 +99,47 @@ export class DetailsProjetComponent implements OnInit{
       this.showForm = !this.showForm;
   }
 
+  getCommentaire() {
+    this.commentaireService.getCommentaire(this.id, 'INFO')
+    .subscribe((resp: any) => {
+      this.commentaire = resp.data;
+
+      for (const comment of this.commentaire) {
+        this.authService.getUserById(comment.id_user)
+          .subscribe((user: any) => {
+            comment.user = user.nom;
+          });
+      }
+    },
+    (error) => {
+      console.error('Une erreur est survenue : ', error);
+    });
+  }
+
+  commenter(){
+    const idUser = localStorage.getItem("idUser");
+    console.log(idUser);
+    
+    if (this.comment) {
+      const commentaire = {
+        type_mere: 'PROJET',
+        id_mere: this.id,
+        id_user: idUser,
+        contenu: this.comment
+      }
+      console.log(commentaire);
+      
+      this.commentaireService.addCommentaire(commentaire)
+      .subscribe(data => {
+        console.log(data);
+      },
+      (error) => {
+        console.error('Une erreur est survenue : ', error);
+      });
+    }
+    else{
+      console.log('Veuillez ajouter un commentaire');
+    }
+  }
 
 }
